1d783b82f06bb6de8adcc045cdb1bea9a25be48c
whendel <whendel.s.a.wsa@gmail.com>
2024-07-29 17:44:07 -0300

    primeiro commit

---
+++ index1.rb
@@ -0,0 +1,9 @@
+print "Digite o primeiro número:"
+numero1= gets.chomp.to_f
+
+print "Digite o segundo número:"
+numero2= gets.chomp.to_f
+
+soma = numero1 + numero2
+
+puts "Á soma dos dois números é :#{soma}"
---
+++ index2.rb
@@ -0,0 +1,7 @@
+print "Qual é o seu nome?"
+nome= gets.chomp
+
+print "qual é a sua idade?"
+idade= gets.chomp.to_f
+
+puts "Bom dia!, #{nome} " "sua idade é: #{idade}"
